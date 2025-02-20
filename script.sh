a4b8dffd24a40527ee06860290d25768de6681b9
StarAssassin64 <starassassin64@starassassin64.com>
2025-02-20 10:48:12 -0600

    Added automated script for removing and linking

---
+++ script.sh
@@ -0,0 +1,4 @@
+rm -r ~/.config/lvim && ln -s ~/.dotfiles/lvim ~/.config/lvim
+rm -r ~/.config/paru && ln -s ~/.dotfiles/paru ~/.config/paru
+rm -r ~/.config/lazygit && ln -s ~/.dotfiles/lazygit ~/.config/lazygit
+rm ~/.zshrc && ln -s ~/.dotfiles/.zshrc_omz ~/.zshrc
